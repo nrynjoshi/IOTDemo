50666f2704555880206ff8a412d985cbba54a9f1
NARAYAN\geeva <geevan.josh@gmail.com>
2024-01-18 22:19:44 +0000

    added JSON file for Rest API response for frontend and expose API

--- app.py
+++ app.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 
 @app.route('/energy')
 def energy_json():
-    response = file_util.read_file(r'C:\Users\geeva\OneDrive - Leeds Beckett University\Dissertation Module\meeting 2\DemoApp\data\energy.json')
+    response = file_util.read_file(r'.\data\energy.json')
+    return response
+
+@app.route('/force-directed-graph')
+def force_directed_graph():
+    response = file_util.read_file(r'.\data\force-directed-graph.json')
+    return response
+
+@app.route('/mobile-patent-suits')
+def mobile_patent_suits():
+    response = file_util.read_file(r'.\data\mobile-patent-suits.json')
+    return response
+
+@app.route('/parallel-coordinate-cars')
+def parallel_coordinate_cars():
+    response = file_util.read_file(r'.\data\parallel-coordinate-cars.json')
+    return response
+
+@app.route('/parallel-coordinate-keys')
+def parallel_coordinate_keys():
+    response = file_util.read_file(r'.\data\parallel-coordinate-keys.json')
     return response
 
 @app.route('/')
