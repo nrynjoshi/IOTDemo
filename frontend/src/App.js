25bb68f84ee7d50567a175cd3a96bad5202200fc
NARAYAN\geeva <geevan.josh@gmail.com>
2024-01-22 07:23:58 +0000

    add react component to render on page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from "react";
 import SankeyDiagram from './view/react-d3/SankeyDiagram';
 import ForceDirectedGraph from './view/react-d3/ForceDirectedGraph';
+import MobilePatentSuits from './view/react-d3/MobilePatentSuits';
 
 class App extends React.Component {
 
@@ -11,7 +12,13 @@ class App extends React.Component {
       <div className="App">
         <div>D3.js with Reactjs Sample Demo App</div>
           <div>
+          
+            <hr></hr>
             <SankeyDiagram></SankeyDiagram> 
+            <hr></hr>
+            <ForceDirectedGraph></ForceDirectedGraph>
+            <hr></hr>
+            <MobilePatentSuits></MobilePatentSuits>
           </div>
            <div>
            </div>
