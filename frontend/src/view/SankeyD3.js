4cd077f1e9da494412565f6b6e864d61e81af640
NARAYAN\geeva <geevan.josh@gmail.com>
2024-01-22 07:22:32 +0000

    added all d3 and diagram consult logic to here

--- frontend/src/view/SankeyD3.js
+++ frontend/src/view/SankeyD3.js
@@ -1,18 +1,24 @@
 import * as d3 from "d3";
 import {sankey as d3Sankey, sankeyLinkHorizontal} from "d3-sankey";
 import * as d3SankeyJS from "d3-sankey";
+import invalidation from "react"
 
 
 const format = d3.format(",.0f");
 
-
+//----------------- supporting methods ---------------
 
 // Create a SVG container.
 export const createSVGContainer = (width, height)=> {
+    const svg = createSVGWithViewBoxCustomContainer(0, 0, width, height);
+            return svg;
+}
+
+export const createSVGWithViewBoxCustomContainer = (viewBoxFirstParam, viewBoxSecondParam, width, height)=> {
     const svg = d3.create("svg")
             .attr("width", width)
             .attr("height", height)
-            .attr("viewBox", [0, 0, width, height])
+            .attr("viewBox", [viewBoxFirstParam, viewBoxSecondParam, width, height])
             .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
             return svg;
 }
@@ -117,4 +123,151 @@ export const defineColorScale = () => {
     const svgContainer = document.getElementById(svgContainerIdToAppend); // Replace with your actual container ID
             svgContainer.innerHTML = "";
             svgContainer.appendChild(svg.node());
+ }
+
+
+ //--------------- diagram generation full code block ---------
+
+ export const SankeyDiagramDisplay = (width, height, data, nodeAlignment, linkColor, svgAppendId) => {
+     // Create a SVG container.
+            
+     const svg = createSVGContainer(width, height);
+
+     // Constructs and configures a Sankey generator.
+     const sankey = constructAndConfiguresSankeyGenerator(width, height, nodeAlignment)
+     
+     // Applies it to the data. We make a copy of the nodes and links objects
+     // so as to avoid mutating the original.
+     const {nodes, links} = sankey({
+     nodes: data.nodes.map(d => Object.assign({}, d)),
+     links: data.links.map(d => Object.assign({}, d))
+     });
+
+ 
+     
+     // Creates the rects that represent the nodes.
+     const rect = createRectsThatRepresentTheNodes(svg, nodes)
+
+     // Adds a title on the nodes.
+     addTitleOnTheNodes(rect)
+     
+     // Creates the paths that represent the links.
+     const link = createThePathsThatRepresentTheLinks(svg, links);
+         
+
+     // Creates a gradient, if necessary, for the source-target color option.
+     createGradientForSourceTargetColor(link, linkColor)
+
+     // Adds labels on the nodes.
+     addLabelsOnTheNodes(svg, nodes, width);
+
+     // Append the SVG to the container in your app.js
+     appendSVGToTheContainer(svg, svgAppendId)    
+ }
+
+ //---------------------
+ function linkArc(d) {
+    const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
+    return `
+      M${d.source.x},${d.source.y}
+      A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
+    `;
+  }
+
+ const drag = (simulation) => {
+  
+    function dragstarted(event, d) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+    
+    function dragged(event, d) {
+      d.fx = event.x;
+      d.fy = event.y;
+    }
+    
+    function dragended(event, d) {
+      if (!event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+    
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+  }
+
+ export const MobilePatentSuitsDisplay = (width, height, data, svgAppendId) => {
+
+    const types = Array.from(new Set(data.map(d => d.type)));
+    const nodes = Array.from(new Set(data.flatMap(l => [l.source, l.target])), id => ({id}));
+    const links = data.map(d => Object.create(d))
+
+    const simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id(d => d.id))
+    .force("charge", d3.forceManyBody().strength(-400))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY());
+
+    // Create a SVG container.
+    const svg = createSVGWithViewBoxCustomContainer (-width / 2, -height / 2, width, height);
+
+    // Per-type markers, as they don't inherit styles.
+    svg.append("defs").selectAll("marker")
+    .data(types)
+    .join("marker")
+        .attr("id", d => `arrow-${d}`)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 15)
+        .attr("refY", -0.5)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+    .append("path")
+        .attr("fill", color)
+        .attr("d", "M0,-5L10,0L0,5");
+
+        const link = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke-width", 1.5)
+      .selectAll("path")
+      .data(links)
+      .join("path")
+        .attr("stroke", d => color(d.type))
+        .attr("marker-end", d => `url(${new URL(`#arrow-${d.type}`, window.location)})`);
+  
+    const node = svg.append("g")
+        .attr("fill", "currentColor")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-linejoin", "round")
+      .selectAll("g")
+      .data(nodes)
+      .join("g")
+        .call(drag(simulation));
+  
+    node.append("circle")
+        .attr("stroke", "white")
+        .attr("stroke-width", 1.5)
+        .attr("r", 4);
+  
+    node.append("text")
+        .attr("x", 8)
+        .attr("y", "0.31em")
+        .text(d => d.id)
+      .clone(true).lower()
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-width", 3);
+  
+    simulation.on("tick", () => {
+      link.attr("d", linkArc);
+      node.attr("transform", d => `translate(${d.x},${d.y})`);
+    });
+  
+    //  invalidation.then(() => simulation.stop());
+
+    // Append the SVG to the container in your app.js
+    appendSVGToTheContainer(svg, svgAppendId) 
  }
