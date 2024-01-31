c7ce7b15eefaa9e60c04c517e112054f11c6af40
NARAYAN\geeva <geevan.josh@gmail.com>
2024-01-31 13:56:52 +0000

    app.js with react component call with API call endpoint as a attributes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import React from "react";
-import SankeyDiagram from './view/react-d3/SankeyDiagram';
-import ForceDirectedGraph from './view/react-d3/ForceDirectedGraph';
-import MobilePatentSuits from './view/react-d3/MobilePatentSuits';
+import SankeyDiagram from './view/component/SankeyDiagram';
+import MobilePatentSuits from './view/component/MobilePatentSuits';
+import ForceDirectedGraph from './view/component/ForceDirectedGraph';
+import ParallelCoordinates from "./view/component/ParallelCoordinates";
+import {BACKEND_API_CALL} from "./view/util/Constant";
+
+
 
 class App extends React.Component {
 
@@ -12,15 +16,13 @@ class App extends React.Component {
       <div className="App">
         <div>D3.js with Reactjs Sample Demo App</div>
           <div>
-          
-            <div>
-            <SankeyDiagram width={928} height={600}></SankeyDiagram>
-            </div>
-            
+            <SankeyDiagram dataEndpoint={BACKEND_API_CALL + '/energy'}></SankeyDiagram>
             <hr></hr>
-            <ForceDirectedGraph  width={928} height={600}></ForceDirectedGraph>
+            <ForceDirectedGraph  dataEndpoint={BACKEND_API_CALL + '/force-directed-graph'}></ForceDirectedGraph>
             <hr></hr>
-            <MobilePatentSuits  width={928} height={600}></MobilePatentSuits>
+            <MobilePatentSuits  dataEndpoint={BACKEND_API_CALL + '/mobile-patent-suits'}></MobilePatentSuits>
+            <hr></hr>
+            <ParallelCoordinates dataEndpoint={BACKEND_API_CALL + '/parallel-coordinate-cars'} keysEndpoint={BACKEND_API_CALL + '/parallel-coordinate-keys'}></ParallelCoordinates>
           </div>
            <div>
            </div>
