346b3619a82b505617410bfdad89ee5feeda2c7e
NARAYAN\geeva <geevan.josh@gmail.com>
2024-03-04 22:11:34 +0000

    router work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import ForceDirectedGraph from './view/component/ForceDirectedGraph';
 import ParallelCoordinates from "./view/component/ParallelCoordinates";
 import {BACKEND_API_CALL} from "./view/util/Constant";
 import PatientDashboard from "./view/component/PatientDashboard";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import ActivityTrack from "./view/component/ActivityTrack";
 import Analysis from "./view/component/Analysis";
 import EmergencyContact from "./view/component/EmergencyContact";
@@ -26,11 +26,11 @@ class App extends React.Component {
 
                 <div className="bg-neutral-500 shadow-lg shadow-cyan-500/50 w-full h-12 header">
                     <ul>
-                        <li><a href="/" className="bg-black">Dashboard</a></li>
-                        <li><a href="/activities-track" >Activities Track</a></li>
-                        <li><a href="/analysis">Analysis</a></li>
-                        <li><a href="/emergency-contacts">Emergency Contacts</a></li>
-                        <li><a href="/inbox">Inbox</a></li>
+                        <li><Link to="/" className="bg-black">Dashboard</Link></li>
+                        <li><Link to="/activities-track" >Activities Track</Link></li>
+                        <li><Link to="/analysis">Analysis</Link></li>
+                        <li><Link to="/emergency-contacts">Emergency Contacts</Link></li>
+                        <li><Link to="/inbox">Inbox</Link></li>
                     </ul>
                 </div>
                 <div className="m-4">
@@ -40,7 +40,6 @@ class App extends React.Component {
                         <Route path="/analysis" element={<Analysis />} />
                         <Route path="/emergency-contacts" element={<EmergencyContact />} />
                         <Route path="/inbox" element={<Inbox />} />
-
                     </Routes>
 
 
