c539bfdec6f860d864704f17ad1d54beab058d74
NARAYAN\geeva <geevan.josh@gmail.com>
2024-03-04 22:02:06 +0000

    create all separate component to handle different menu

---
+++ frontend/src/view/component/Analysis.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PageTitle from "./PageTitle";
+
+class Analysis extends React.Component {
+
+    state = {dataCar: null, dataKeys:null, colorEncoding: "weight (lb)", httpErrorMessage: null, isLoading: false};
+
+    handleColorEncodingChange = (e) => {
+        console.log('handleColorEncodingChange called');
+        this.setState({colorEncoding: e.target.value});
+    }
+
+
+    render() {
+        const {httpErrorMessage, colorEncoding,isLoading, dataCar} = this.state
+        return (<div>
+
+            {isLoading ? (<p>Loading ...</p>) : (httpErrorMessage ?
+                <h2 style={{backgroundColor: 'red'}}>{httpErrorMessage}</h2> : <span></span>)}
+<PageTitle title="Analysis"></PageTitle>
+
+
+        </div>);
+    }
+}
+
+
+export default Analysis;
