b085ff9e7648ed8f19ee04c7395463fb7e2bc497
NARAYAN\geeva <geevan.josh@gmail.com>
2024-03-09 20:37:22 +0000

    emergency contact component added and menu select css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,36 +9,40 @@ import EmergencyContact from "./view/component/EmergencyContact";
 import Inbox from "./view/component/Inbox";
 import Demo from "./view/component/Demo";
 import PageNotFound from "./view/component/PageNotFound";
+import {BACKEND_API_CALL} from "./view/util/Constant";
 
 class App extends React.Component {
 
 
   render(){
 
+
     return (
          <BrowserRouter>
-             <div>
-                 <div className="bg-neutral-500 shadow-lg shadow-cyan-500/50 w-full h-12 header">
+             <div className="w-full text-2xl App">
+                 <div className="header">
                      <ul>
-                         <li><NavLink to="/" className={({ isActive }) => { return isActive ? "bg-black" : "text-white";}}>Dashboard</NavLink></li>
-                         <li><NavLink to="/activities-track" className={({ isActive }) => { return isActive ? "bg-black" : "text-white";}}>Activities Track</NavLink></li>
-                         <li><NavLink to="/analysis" className={({ isActive }) => { return isActive ? "bg-black" : "text-white";}}>Analysis</NavLink></li>
-                         <li><NavLink to="/emergency-contacts" className={({ isActive }) => { return isActive ? "bg-black" : "text-white";}}>Emergency Contacts</NavLink></li>
-                         <li><NavLink to="/inbox" className={({ isActive }) => { return isActive ? "bg-black" : "text-white";}}>Inbox</NavLink></li>
-                         <li><NavLink to="/demo" className={({ isActive }) => { return isActive ? "bg-black" : "text-white";}}>Demo</NavLink></li>
+                         <li><NavLink to="/" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Dashboard</NavLink></li>
+                         <li><NavLink to="/activities-track" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Activities Track</NavLink></li>
+                         <li><NavLink to="/analysis" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Analysis</NavLink></li>
+                         <li><NavLink to="/emergency-contacts" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Emergency Contacts</NavLink></li>
+                         <li><NavLink to="/inbox" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Inbox</NavLink></li>
+                         <li><NavLink to="/demo" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Demo</NavLink></li>
                      </ul>
 
                  </div>
+                 <div >
                  <div className="m-4">
                      <Routes>
                          <Route path="/" element={<PatientDashboard/>}/>
                          <Route path="/activities-track" element={<ActivityTrack/>}/>
                          <Route path="/analysis" element={<Analysis/>}/>
-                         <Route path="/emergency-contacts" element={<EmergencyContact/>}/>
+                         <Route path="/emergency-contacts" element={<EmergencyContact dataEndpoint={BACKEND_API_CALL + '/emergency-contacts'}/>}/>
                          <Route path="/inbox" element={<Inbox/>}/>
                          <Route path="/demo" element={<Demo/>}/>
                         <Route element={<PageNotFound/>}/>
                       </Routes>
+                 </div>
                  </div>
 
 
