3030909c09a14d389657f18234c42ffec1f5327d
NARAYAN\geeva <geevan.josh@gmail.com>
2024-03-09 20:36:48 +0000

    emergency contacts component added

--- frontend/src/view/component/EmergencyContact.js
+++ frontend/src/view/component/EmergencyContact.js
@@ -1,27 +1,64 @@
+import ErrorBoundary from "../util/ErrorBoundary.js"
 import React from "react";
-import PageTitle from "./PageTitle";
+import HttpClient from "../util/HttpClient"
+
 
 class EmergencyContact extends React.Component {
 
-    state = {dataCar: null, dataKeys:null, colorEncoding: "weight (lb)", httpErrorMessage: null, isLoading: false};
+    state = {data: null, httpErrorMessage:null, isLoading: false};
 
-    handleColorEncodingChange = (e) => {
-        console.log('handleColorEncodingChange called');
-        this.setState({colorEncoding: e.target.value});
+    componentDidMount() {
+        const url = this.props.dataEndpoint;
+        //calling api for data
+        const fetchData = async () => {
+            try {
+                this.setState({isLoading: true})
+              const data =  await HttpClient.get(url);
+              this.setState({data:data, httpErrorMessage:null});
+            } catch (error) {
+              console.error('Error fetching data:', error);
+              this.setState({data:null, httpErrorMessage: JSON.stringify(error.toString()) });
+            } finally {
+                this.setState({isLoading: false})
+            }
+          };
+
+          fetchData();
+
     }
 
 
-    render() {
-        const {httpErrorMessage, colorEncoding,isLoading, dataCar} = this.state
-        return (<div>
 
-            {isLoading ? (<p>Loading ...</p>) : (httpErrorMessage ?
-                <h2 style={{backgroundColor: 'red'}}>{httpErrorMessage}</h2> : <span></span>)}
-<PageTitle title="Emergency Contact"></PageTitle>
+     render(){
+        const { httpErrorMessage, isLoading, data} = this.state
 
+         const handleChange = () => {
+            console.log("clicked checkbox")
+          };
 
-        </div>);
-    }
+            return (
+            <div><ErrorBoundary>
+
+                {isLoading ? (
+                    <p>Loading ...</p>
+                ) : (httpErrorMessage ? <h2 style={{backgroundColor: 'red'}}>{httpErrorMessage}</h2>:
+                <span></span>
+                )}
+                {data && data.map((emergencyContact) => (
+
+                    <div className="bg-white mb-3">
+                        <div>Name: {emergencyContact.name}</div>
+                        <div>Contact Number: {emergencyContact.contact_number}</div>
+                        <div>Address: {emergencyContact.address}</div>
+                        <div>Relationship: {emergencyContact.relationship}</div>
+                        <div>Action: <a href="tel:55">Call (work left)</a></div>
+                    </div>
+                )
+                    )}
+
+            </ErrorBoundary></div>
+            );
+     }
 }
 
 
