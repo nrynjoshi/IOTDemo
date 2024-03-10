f8ec6261d06ed237fb54447d88ac6352e9cb0c7c
NARAYAN\geeva <geevan.josh@gmail.com>
2024-03-10 23:18:29 +0000

    activities tracking work inprogress

--- frontend/src/view/component/Analysis.js
+++ frontend/src/view/component/Analysis.js
@@ -17,8 +17,49 @@ class Analysis extends React.Component {
 
             {isLoading ? (<p>Loading ...</p>) : (httpErrorMessage ?
                 <h2 style={{backgroundColor: 'red'}}>{httpErrorMessage}</h2> : <span></span>)}
-<PageTitle title="Analysis"></PageTitle>
 
+
+<div>
+                        <div className="grid grid-cols-2 gap-2">
+
+                            <div className="bg-white shadow-lg shadow-grey-500/50 min-w-6 min-h-6">
+                            <PageTitle title="Heart Rate Analysis"></PageTitle>
+                            charts goes here
+
+                            </div>
+
+                            <div className="bg-white shadow-lg shadow-grey-500/50 min-w-6 min-h-6">
+                            <PageTitle title="SPO2 Analysis"></PageTitle>
+                            charts goes here
+
+                            </div>
+
+                            <div className="bg-white shadow-lg shadow-grey-500/50 min-w-6 min-h-6">
+                            <PageTitle title="Blood Sugar Analysis"></PageTitle>
+                            charts goes here
+
+                            </div>
+
+                            <div className="bg-white shadow-lg shadow-grey-500/50 min-w-6 min-h-6">
+                            <PageTitle title="Blood Temperature Analysis"></PageTitle>
+                            charts goes here
+
+                            </div>
+
+                            <div className="bg-white shadow-lg shadow-grey-500/50 min-w-6 min-h-6">
+                            <PageTitle title="Sleep Pattern Analysis"></PageTitle>
+                            charts goes here
+
+                            </div>
+
+                            <div className="bg-white shadow-lg shadow-grey-500/50 min-w-6 min-h-6">
+                            <PageTitle title="Steps Analysis"></PageTitle>
+                            charts goes here
+
+                            </div>
+
+                        </div>
+                    </div>
 
         </div>);
     }
