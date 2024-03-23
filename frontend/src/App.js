77171b6c813185184499ba4cebba2083ea62adf2
NARAYAN\geeva <geevan.josh@gmail.com>
2024-03-23 23:19:56 +0000

    remove unwanted linked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,16 @@ import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
 import ActivityTrack from "./view/component/ActivityTrack";
 import Analysis from "./view/component/Analysis";
 import EmergencyContact from "./view/component/EmergencyContact";
-import Inbox from "./view/component/Inbox";
 import Demo from "./view/component/Demo";
 import PageNotFound from "./view/component/PageNotFound";
 import {BACKEND_API_CALL} from "./view/util/Constant";
 import DateTime from './view/util/DateTime';
+import TodoList from './view/component/TodoList';
 
 class App extends React.Component {
 
 
   render(){
-
 
     return (
          <BrowserRouter>
@@ -24,24 +23,25 @@ class App extends React.Component {
                  <div className="header">
                      <ul>
                          <li><NavLink to="/" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Dashboard</NavLink></li>
+                         <li><NavLink to="/todo-list" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>TODO List</NavLink></li>
                          <li><NavLink to="/activities-track" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Activities Track</NavLink></li>
                          <li><NavLink to="/analysis" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Analysis</NavLink></li>
                          <li><NavLink to="/emergency-contacts" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Emergency Contacts</NavLink></li>
-                         <li><NavLink to="/inbox" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Inbox</NavLink></li>
                          <li><NavLink to="/demo" className={({ isActive }) => { return isActive ? "bg-black font-bold" : "text-white";}}>Demo</NavLink></li>
+                        <li className='datetime'><DateTime></DateTime></li>
                      </ul>
                      
                  </div>
                  <div >
-                 <div className="text-right"><DateTime></DateTime></div>
+                
                  <div className="m-4">
                  
                      <Routes>
                          <Route path="/" element={<PatientDashboard/>}/>
+                         <Route path="/todo-list" element={<TodoList dataEndpoint= {BACKEND_API_CALL + '/todo-list'}/>}/>
                          <Route path="/activities-track" element={<ActivityTrack/>}/>
                          <Route path="/analysis" element={<Analysis/>}/>
                          <Route path="/emergency-contacts" element={<EmergencyContact dataEndpoint={BACKEND_API_CALL + '/emergency-contacts'}/>}/>
-                         <Route path="/inbox" element={<Inbox/>}/>
                          <Route path="/demo" element={<Demo/>}/>
                         <Route element={<PageNotFound/>}/>
                       </Routes>
