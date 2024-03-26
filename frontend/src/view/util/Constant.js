385c3ac03e219ef2e73fd5b27742a19f8d6d87e4
NARAYAN\geeva <geevan.josh@gmail.com>
2024-03-26 22:59:51 +0000

    url control by env so that after build, it will access from python directly

--- frontend/src/view/util/Constant.js
+++ frontend/src/view/util/Constant.js
@@ -1,2 +1,2 @@
-export const BACKEND_API_CALL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL;
+export const BACKEND_API_CALL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_DEV_API_URL;
 
