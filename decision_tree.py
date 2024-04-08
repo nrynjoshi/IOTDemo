71635239bf88ecd488733924c9d50ec2fa293af7
NARAYAN\geeva <geevan.josh@gmail.com>
2024-04-08 22:25:26 +0100

    cleanup

--- decision_tree.py
+++ decision_tree.py
@@ -5,43 +5,58 @@ from sklearn.model_selection import train_test_split
 
 import azure_blob_util
 
-# Step 2: Load Dataset
-json_list = azure_blob_util.getRulesContainer('Disease Symptoms and Patient Profile Dataset.csv')
-
-# Convert list of JSON objects to DataFrame
-data = pd.DataFrame(json_list)
-
-# Step 3: Data Preprocessing (if needed)
-# Convert 'yes' and 'no' values to binary (0 or 1)
-
-data['Fever'] = data['Fever'].map({'yes': 1, 'no': 0})
-data['Cough'] = data['Cough'].map({'yes': 1, 'no': 0})
-data['Fatigue'] = data['Fatigue'].map({'yes': 1, 'no': 0})
-data['Difficulty Breathing'] = data['Difficulty Breathing'].map({'yes': 1, 'no': 0})
-data['Blood Pressure'] = data['Blood Pressure'].map({'Low': -1, 'Normal': 0, 'High': 1})
-data['Cholesterol Level'] = data['Cholesterol Level'].map({'Low': -1, 'Normal': 0, 'High': 1})
-data['Outcome Variable'] = data['Outcome Variable'].map({'Positive': 1, 'Negative': 0})
-data['Gender'] = data['Gender'].map({'Male': 1, 'Female': 0})
-
-# Step 4: Split Data into Features and Labels
-X = data.drop(['Disease', 'Outcome Variable'], axis=1)  # Features
-y_disease = data['Disease']  # Label for Disease
-y_outcome = data['Outcome Variable']  # Label for Outcome Variable
-
-# Step 5: Train Decision Tree Model for Disease
-X_train, X_test, y_disease_train, y_disease_test = train_test_split(X, y_disease, test_size=0.2, random_state=42)
 clf_disease = DecisionTreeClassifier()
-clf_disease.fit(X_train, y_disease_train)
-
-# Step 6: Train Decision Tree Model for Outcome Variable
-X_train, X_test, y_outcome_train, y_outcome_test = train_test_split(X, y_outcome, test_size=0.2, random_state=42)
 clf_outcome = DecisionTreeClassifier()
-clf_outcome.fit(X_train, y_outcome_train)
+isInitDone: bool = False
 
 
-def process(has_fever: str, has_cough: str, has_fatigue: str, has_difficulty_breathing: str, age: int, is_male: str, blood_pressure_level: str,
+def init():
+    # Step 2: Load Dataset
+    json_list = azure_blob_util.getRulesContainer('Disease Symptoms and Patient Profile Dataset.csv')
+
+    # Convert list of JSON objects to DataFrame
+    data = pd.DataFrame(json_list)
+
+    # Step 3: Data Preprocessing (if needed)
+    # Convert 'yes' and 'no' values to binary (0 or 1)
+
+    data['Fever'] = data['Fever'].map({'yes': 1, 'no': 0})
+    data['Cough'] = data['Cough'].map({'yes': 1, 'no': 0})
+    data['Fatigue'] = data['Fatigue'].map({'yes': 1, 'no': 0})
+    data['Difficulty Breathing'] = data['Difficulty Breathing'].map({'yes': 1, 'no': 0})
+    data['Blood Pressure'] = data['Blood Pressure'].map({'Low': -1, 'Normal': 0, 'High': 1})
+    data['Cholesterol Level'] = data['Cholesterol Level'].map({'Low': -1, 'Normal': 0, 'High': 1})
+    data['Outcome Variable'] = data['Outcome Variable'].map({'Positive': 1, 'Negative': 0})
+    data['Gender'] = data['Gender'].map({'Male': 1, 'Female': 0})
+
+    # Step 4: Split Data into Features and Labels
+    X = data.drop(['Disease', 'Outcome Variable'], axis=1)  # Features
+    y_disease = data['Disease']  # Label for Disease
+    y_outcome = data['Outcome Variable']  # Label for Outcome Variable
+
+    # Step 5: Train Decision Tree Model for Disease
+    X_train, X_test, y_disease_train, y_disease_test = train_test_split(X, y_disease, test_size=0.2, random_state=42)
+    global clf_disease
+    clf_disease.fit(X_train, y_disease_train)
+
+    # Step 6: Train Decision Tree Model for Outcome Variable
+    X_train, X_test, y_outcome_train, y_outcome_test = train_test_split(X, y_outcome, test_size=0.2, random_state=42)
+    global clf_outcome
+    clf_outcome.fit(X_train, y_outcome_train)
+    global isInitDone
+    isInitDone = True
+
+
+def process(has_fever: str, has_cough: str, has_fatigue: str, has_difficulty_breathing: str, age: int,
+            is_male: str, blood_pressure_level: str,
             cholesterol_level: str):
+    global clf_disease
+    global clf_outcome
+    global isInitDone
+
     # Step 7: Make Predictions
+    if not isInitDone:
+        init()
 
     def convert_bool_to_int(b):
         if b.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -77,6 +92,7 @@ def process(has_fever: str, has_cough: str, has_fatigue: str, has_difficulty_bre
             return 'Positive'
         else:
             return 'Negative'
+
     print("Predicted Disease:", predicted_disease)
     print("Predicted Outcome Variable:", predicted_outcome)
     outcome = {'disease': predicted_disease[0], 'outcome': convert_outcome_to_string(predicted_outcome)}
