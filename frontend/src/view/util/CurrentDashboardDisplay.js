312091827f6684958c95ac0a593f7fe80edb94d9
NARAYAN\geeva <geevan.josh@gmail.com>
2024-04-19 10:16:11 +0100

    added speedometer and displayed dashboard record using speedometer

--- frontend/src/view/util/CurrentDashboardDisplay.js
+++ frontend/src/view/util/CurrentDashboardDisplay.js
@@ -1,6 +1,7 @@
 import ErrorBoundary from "../util/ErrorBoundary.js"
 import React from "react";
 import SoundUtil from "../util/SoundUtil";
+import SpeedometerD3 from "../react-d3/SpeedometerD3.js";
 
 
 
@@ -15,13 +16,21 @@ class CurrentDashboardDisplay extends React.Component {
           const displayValue = this.props.displayValue;
           const minNormalValue = this.props.minNormalValue;
           const maxNormalValue = this.props.maxNormalValue;
+          const customSegmentStops =  this.props.customSegmentStops;
+          const customSegmentLabels =  this.props.customSegmentLabels;
           return (
                <div><ErrorBoundary>
 
                     <div>
                          <h2>{displayText}</h2>
                          <div className="flex items-center">
-                              <span className="text-red-500 text-6xl font-bold text-center p-3">{displayValue}</span>
+                         {customSegmentStops ? (
+                              <SpeedometerD3 value ={displayValue} customSegmentStops ={customSegmentStops} customSegmentLabels= {customSegmentLabels}/>
+                              ) : (
+                                   <span className="text-red-500 text-6xl font-bold text-center p-3">{displayValue}</span>
+                              )}
+                              
+                              
                               <div>
                                    <SoundUtil value={displayValue} minNormalValue={minNormalValue} maxNormalValue={maxNormalValue}></SoundUtil>
                               </div>
