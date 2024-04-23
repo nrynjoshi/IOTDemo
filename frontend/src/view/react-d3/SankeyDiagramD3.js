54e026102071160a316e8fcb519b7e20a8588cab
NARAYAN\geeva <geevan.josh@gmail.com>
2024-04-23 15:46:33 +0100

    more colors added for Sankey and force-directed graphs visualization

--- frontend/src/view/react-d3/SankeyDiagramD3.js
+++ frontend/src/view/react-d3/SankeyDiagramD3.js
@@ -8,7 +8,10 @@ export const createSankeyDiagramSVG = (data, linkColor, nodeAlignment) => {
     const width = 1200;
     const height = 700;
     const format = d3.format(",.0f");
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    const color = d3.scaleOrdinal()
+    .range([ "#C0C0C0", "#808080", "#FF0000", "#800000", "#FFFF00", "#808000", "#00FF00", "#008000", "#00FFFF",
+     "#008080", "#0000FF", "#000080", "#FF00FF", "#800080", "#CD5C5C", "#F08080", "#E9967A", "#FFA07A", "#DFFF00", "#6495ED", "#CCCCFF", "#40E0D0", "#9FE2BF"
+     , "#800980", "#CD525C", "#F08580", "#E9867A", "#FF507A", "#DF3F00", "#6415ED", "#CCC1FF", "#4010D0", "#9FE2AF"]);
 
     // Create a SVG container.
     const svg = d3.create("svg")
